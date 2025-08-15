cf3fcb5bca3b0d2c66dbe0fa193ba747b7cd6137
marlenekallass <marlene.kallass@gmail.com>
2025-08-15 19:53:36 +0200

    Updated phase encoding

--- code/R/Chapter 2/phase_encoding_with_plots.R
+++ code/R/Chapter 2/phase_encoding_with_plots.R
@@ -480,9 +480,10 @@ p_object = ggplot(df, aes(x = x, y = y, fill = val)) +
   theme_void() +
   theme(legend.position = "none")
 
-
+amp_sum = sum(mat)
+n = 5
 # Time interval
-#Test with freq_0 = 1, instead of 2
+
 delta_f_max = 1
 period = 1/delta_f_max
 
@@ -504,8 +505,6 @@ delta_f_FOV = (n-1)*delta_f
 freq_x = t(sapply(delta_f_FOV, function(f) seq(0, f, length.out = n)))
 
 
-#amp_sum = sum(mat)
-#amp_freq = numeric(0)
 kspace = numeric(0)
 plots = list()
 for (f_idx in 1:n_samples){
@@ -528,7 +527,7 @@ for (f_idx in 1:n_samples){
   p_amp = ggplot(df, aes(x, y)) +
     geom_line() +
     theme_minimal() +
-    labs(x = expression(Delta*"f [Hz]"), y = "Value at time = T")+
+    labs(x = expression(Delta*"f [Hz]"), y = "Signal at time = T")+
     xlim(delta_f[1],delta_f_max)+
     coord_fixed(ratio = 1/12) +
     ylim(-amp_sum/2, amp_sum)
@@ -548,7 +547,10 @@ p_gradient = ggplot(df, aes(x, y)) +
   ggtitle("Phase gradient")+
   theme(plot.title = element_text(size = 10))
   
-plots[[f_idx]] = wrap_plots(p_object,p_amp,p_gradient,ncol = 2,nrow = 2)
+plots[[f_idx]] = wrap_plots(p_object,p_amp,p_gradient,ncol = 2,nrow = 2) & theme(
+  plot.background  = element_rect(fill = "transparent", colour = NA),
+  panel.background = element_rect(fill = "transparent", colour = NA)
+  )
 
 }
 
@@ -557,5 +559,59 @@ plots_gif = plots[seq(1, length(plots), by = 10)]
 
 filename = "phase_encoding_object_demo_test.gif"
 path_out = file.path(path_figures, filename)
-create_gif_from_plots(plots_gif, path_out, 800, 400, 100, fps = 1)
+create_gif_from_plots(plots_gif, path_out, 1800, 900, 225, fps = 1)
+
+
+## Actual phase encoding ##
+
+delta_f_max = 1
+
+T_grad = 1/(2*delta_f_max)
+
+delta_f_steps = delta_f_max/(n-1)
+
+delta_f = seq(-delta_f_max,delta_f_max,delta_f_steps)
+
+n_samples = length(delta_f)
+
+delta_f_FOV = (n-1)*delta_f
+
+# How frequency varies in space (1 to n px) depending on gradient strength 
+freq_x = t(sapply(delta_f_FOV, function(f) seq(0, f, length.out = n)))
+
+
+kspace = numeric(n_samples)
+
+for (f_idx in 1:n_samples){
+  
+  
+  signals_px = numeric(n^2)
+  idx = 1
+  for (row_idx in 1:n) {
+    for (col_idx in 1:n) {
+      
+      amp = mat[row_idx, col_idx]
+      
+      phase = 2* freq_x[f_idx,col_idx]*T_grad
+      signals_px[idx] = amp*cos(pi*phase)
+      idx = idx+1
+      
+    }
+  }
+  
+  kspace[f_idx] = sum(signals_px)
+  
+}
+
+
+fft_result = abs(fft(fftshift(kspace)))
+
+# Take only positive frequencies
+img_rec = fft_result[1: (length(fft_result)/2+1)] 
+
+# For correct orientation when plotting
+img_rec_plot = matrix(rep(img_rec, 2), ncol = 2, byrow = FALSE)
+
+image(img_rec_plot, col=gray(seq(0, 1, length=256)),
+      axes=FALSE, useRaster=TRUE,asp=1/(2*length(img_rec)))
 
